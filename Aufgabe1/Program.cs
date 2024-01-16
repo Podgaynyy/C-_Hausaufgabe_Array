36eca5a23c55f213a66ea162f6c357697a9994ae
Podgaynyy <149233360+Podgaynyy@users.noreply.github.com>
2024-01-16 20:20:53 +0100

    Update Program.cs

--- Aufgabe1/Program.cs
+++ Aufgabe1/Program.cs
@@ -1,25 +1,25 @@
-﻿//Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100.
-//Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+//Aufgabe 1: Geben Sie ein eindimensionales Array mit 10 Ganzzahlen von 1 bis 100 an.
+//Finden Sie die Anzahl der Array-Elemente, deren Werte im Intervall [20,90] liegen.
 
-namespace Aufgabe_one;         // это создаёт проект. Не знаю, надо ли это, как в java
+namespace Aufgabe_one;         // Dadurch wird das Projekt erstellt. Ich weiß nicht, ob das notwendig ist, wie in Java
 internal class Program
 {
     static void Main(string[] args)
     {
-        Random zufall = new Random();  //зачем-то заранее сообщаем, что будем работать
-                                       //с рандомными значениями. Если под это нужно
-                                       //выделить память, то мы не сказали, какую памать выделим
-        int[] array = new int[10];  //создали массив
+        Random zufall = new Random();  //Aus irgendeinem Grund informieren wir Sie im Voraus darüber, dass wir arbeiten werden
+                                       //mit zufälligen Werten. Wenn du es brauchst
+                                       //Speicher zuweisen, dann haben wir nicht gesagt, welchen Speicher wir zuweisen werden
+        int[] array = new int[10];  //ein Array erstellt
         for (int i = 0; i < array.Length; i++)
         {
-            array[i] = zufall.Next(0, 101); //заполнили массив рандомными значениями
+            array[i] = zufall.Next(0, 101); //das Array mit Zufallswerten gefüllt
         }
-        int count = 0;  // здесь будем считать количество выполненных условий
-        for (int i = 0; i < array.Length; i++) // опять пройдёмся по массиву
+        int count = 0;  // hier zählen wir die Anzahl der erfüllten Bedingungen
+        for (int i = 0; i < array.Length; i++) // Lass uns das Array noch einmal durchgehen
         {
-            if (array[i] > 20 && array[i] < 80) //если условие выполнится
+            if (array[i] > 20 && array[i] < 80) //wenn die Bedingung erfüllt ist
             {
-                count++; // увеличиваем счёт
+                count++; // Erhöhe die Punktzahl
             }
             else { continue; }
         }
